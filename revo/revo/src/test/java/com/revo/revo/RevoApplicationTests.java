90f14b6d69e6ae2e7b633f1c7928eb5fb94f392e
EsterGNN <gomes.ester23@gmail.com>
2021-08-27 09:51:30 -0300

    Projeto Spring criado.

---
+++ revo/revo/src/test/java/com/revo/revo/RevoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.revo.revo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RevoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
