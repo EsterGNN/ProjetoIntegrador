db075502580bcea7917219c0551daa633c0974a9
EsterGNN <gomes.ester23@gmail.com>
2021-08-27 16:33:19 -0300

    Banco de dados e tabela Tema criados.

--- revo/revo/src/main/java/com/revo/revo/model/Tema.java
+++ revo/revo/src/main/java/com/revo/revo/model/Tema.java
@@ -1,5 +1,74 @@
 package com.revo.revo.model;
 
-public class Tema {
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 
+import com.sun.istack.NotNull;
+
+
+@Entity
+@Table(name = "tb_tema")
+public class Tema {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	
+	@NotNull
+	@Size(max = 100)
+	private String tipo;
+	
+	
+	@NotNull
+	@Size(max = 500)
+	private String descricao;
+	
+	
+	private boolean status = true;
+
+	//GETTERS E SETTERS
+	public long getId() {
+		return id;
+	}
+
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+
+	public String getTipo() {
+		return tipo;
+	}
+
+
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+
+	public boolean isStatus() {
+		return status;
+	}
+
+
+	public void setStatus(boolean status) {
+		this.status = status;
+	}
+	
+	
 }
